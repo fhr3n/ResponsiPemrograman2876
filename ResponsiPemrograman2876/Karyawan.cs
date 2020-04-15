28639c9f112f2c8d9d5113f149e5773ea1c5abf7
fhr3n <56928892+fhr3n@users.noreply.github.com>
2020-04-15 14:51:20 +0700

    Update Karyawan.cs

--- ResponsiPemrograman2876/Karyawan.cs
+++ ResponsiPemrograman2876/Karyawan.cs
@@ -16,7 +16,7 @@ namespace ResponsiPemrograman2876
             this.nama=nama;
             if(gajibulanan<0){
                 this.nik="0";
-                Console.WriteLine("Maaf gaji yang anda diinputkan tidak boleh dari 0");
+                Console.WriteLine("Maaf gaji yang anda inputkan tidak boleh dari 0");
             }
             else{
             this.gajibulanan=gajibulanan;
