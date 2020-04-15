8d1edbe342feba4c420d09999664d9e869ce283a
fhr3n <56928892+fhr3n@users.noreply.github.com>
2020-04-15 15:30:51 +0700

    Update Program.cs

--- ResponsiPemrograman2876/Program.cs
+++ ResponsiPemrograman2876/Program.cs
@@ -16,14 +16,14 @@ namespace ResponsiPemrograman2876
             Console.WriteLine("No Nik/nama        Gaji Bulanan");
             Console.WriteLine("===============================");
             Console.WriteLine("1. " + karyawan1.nik + " " + karyawan1.nama + "\t" + karyawan1.gajibulanan);
-            Console.WriteLine("1. " + karyawan2.nik + " " + karyawan2.nama + "\t\t" + karyawan2.gajibulanan);
+            Console.WriteLine("2. " + karyawan2.nik + " " + karyawan2.nama + "\t\t" + karyawan2.gajibulanan);
             Console.WriteLine("\n\nAsyiiiiik Ada Kenaikan Gaji 10%!!\n");
             Console.WriteLine("No Nik/nama        Gaji Bulanan");
             Console.WriteLine("===============================");
             g1 = (10 * karyawan1.gajibulanan) / 100;
             g2 = (10 * karyawan2.gajibulanan) / 100;
             Console.WriteLine("1. " + karyawan1.nik + " " + karyawan1.nama + "\t" + (g1 + karyawan1.gajibulanan));
-            Console.WriteLine("1. " + karyawan2.nik + " " + karyawan2.nama + "\t\t" + (g2 + karyawan2.gajibulanan));
+            Console.WriteLine("2. " + karyawan2.nik + " " + karyawan2.nama + "\t\t" + (g2 + karyawan2.gajibulanan));
 
             Console.ReadKey();
         }
